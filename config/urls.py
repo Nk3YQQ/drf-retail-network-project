74fa91cc81a1a27aeefa1246d2db1d8ac3f50c21
egor <ega.nik5674@gmail.com>
2024-07-21 23:18:57 +0300

    Update main workflow and urls.py

--- config/urls.py
+++ config/urls.py
@@ -17,13 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    # API
-    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/v1/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
-    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
